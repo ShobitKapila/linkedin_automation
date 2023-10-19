ce76296a036ebb4bfd5ffa5ff8ca77ba4c378b34
ShobitKapila <kapilashobit5@gmail.com>
2023-10-19 20:23:56 +0530

    commit

--- script.js
+++ script.js
@@ -1,29 +1,79 @@
-
-```javascript
-// Define a function to handle the login form submission
-document.getElementById("login-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
-    // Perform the LinkedIn login/authentication (you can use your authentication logic here)
-    // ...
-
-    // You can redirect the user to the search section or perform other actions as needed
-});
-
-// Define a function to handle the search form submission
-document.getElementById("search-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const domain = document.getElementById("domain").value;
-    const pages = document.getElementById("pages").value;
-
-    // Perform the LinkedIn search (you can use your LinkedIn scraping logic here)
-    // ...
-
-    // Display the search results in the "results-container"
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+    const searchForm = document.getElementById("search-form");
     const resultsContainer = document.getElementById("results-container");
 
-    // Example: Append results to the container
-    resultsContainer.innerHTML = `<p>Search results will appear here...</p>`;
+    // Replace this with your actual LinkedIn API endpoint
+    const apiEndpoint = "https://example.com/linkedin-api";
+
+    loginForm.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        try {
+            // Simulate a login request; replace this with your actual LinkedIn authentication logic
+            const response = await fetch(apiEndpoint + "/login", {
+                method: "POST",
+                body: JSON.stringify({ username, password }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (response.ok) {
+                console.log("Logged in successfully");
+                // Redirect to the search section or perform other actions
+            } else {
+                console.error("Login failed");
+            }
+        } catch (error) {
+            console.error("Error during login:", error);
+        }
+    });
+
+    searchForm.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        const domain = document.getElementById("domain").value;
+        const pages = document.getElementById("pages").value;
+
+        try {
+            // Simulate a search request; replace this with your actual LinkedIn scraping logic
+            const response = await fetch(apiEndpoint + "/search", {
+                method: "POST",
+                body: JSON.stringify({ domain, pages }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                displaySearchResults(data);
+            } else {
+                console.error("Search failed");
+            }
+        } catch (error) {
+            console.error("Error during search:", error);
+        }
+    });
+
+    function displaySearchResults(results) {
+        resultsContainer.innerHTML = ""; // Clear previous results
+
+        if (results.length === 0) {
+            resultsContainer.innerHTML = "<p>No results found.</p>";
+        } else {
+            results.forEach((result, index) => {
+                const listItem = document.createElement("div");
+                listItem.classList.add("result-item");
+                listItem.innerHTML = `
+                    <h3>${result.name}</h3>
+                    <p>${result.title}</p>
+                    <p>${result.location}</p>
+                `;
+                resultsContainer.appendChild(listItem);
+            });
+        }
+    }
 });
